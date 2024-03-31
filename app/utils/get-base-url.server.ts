c5aed5ccfc987178517aa794af1d61f253d002e2
André König <andre.koenig@openformation.io>
2024-03-31 20:34:46 +0200

    fix: adds correct protocol to URL

--- app/utils/get-base-url.server.ts
+++ app/utils/get-base-url.server.ts
@@ -1,5 +1,10 @@
 export function getBaseUrl(request: Request) {
-  const url = new URL(request.url)
+  const host =
+    request.headers.get("X-Forwarded-Host") ??
+    request.headers.get("Host") ??
+    new URL(request.url).host;
 
-  return url.origin
+  const protocol = host.includes("localhost") ? "http" : "https";
+
+  return `${protocol}://${host}`;
 }
