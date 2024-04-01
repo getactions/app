e853d3d7d135552292a2cdb496fcc90d2895ad9f
André König <andre.koenig@openformation.io>
2024-04-01 13:59:53 +0200

    fix: hydration error

--- app/routes/_frontend.$category/route.tsx
+++ app/routes/_frontend.$category/route.tsx
@@ -29,27 +29,20 @@ export default function Index() {
   return (
     <>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-        {loaderData.workflows.map((workflow) => {
-          const detailsPath = `/${workflow.category}/${workflow.name}/details`;
-
-          return (
-            <Link key={workflow.id} to={detailsPath}>
-              <WorkflowCard
-                key={workflow.id}
-                workflow={workflow}
-                footer={
-                  <Link
-                    className="text-primary text-sm"
-                    prefetch="intent"
-                    to={detailsPath}
-                  >
-                    Use Workflow
-                  </Link>
-                }
-              />
-            </Link>
-          );
-        })}
+        {loaderData.workflows.map((workflow) => (
+          <Link
+            key={workflow.id}
+            to={`/${workflow.category}/${workflow.name}/details`}
+            prefetch="intent"
+          >
+            <WorkflowCard
+              workflow={workflow}
+              footer={
+                <span className="text-primary text-sm">Use Workflow</span>
+              }
+            />
+          </Link>
+        ))}
       </div>
       <Outlet />
     </>
