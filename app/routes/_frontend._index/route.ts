57bb799dcc30443286cdb8101cad026a5f1b6920
André König <andre.koenig@openformation.io>
2024-04-01 19:56:26 +0200

    chore: make `deployment` index route

--- app/routes/_frontend._index/route.ts
+++ app/routes/_frontend._index/route.ts
@@ -1,9 +1,5 @@
-import { getCategories } from "#workflows";
 import { redirect } from "@remix-run/react";
 
 export async function loader() {
-  // Get first category
-  const category = (await getCategories()).at(0);
-
-  return redirect(`/${category?.id}`);
+  return redirect("/deployment");
 }
