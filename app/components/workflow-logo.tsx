2d733246c1465a338b47fea01753bbb631e17415
André König <andre.koenig@openformation.io>
2024-04-14 11:19:16 +0200

    feat: adds svg file extension to logo URL

--- app/components/workflow-logo.tsx
+++ app/components/workflow-logo.tsx
@@ -6,7 +6,7 @@ type Props = Readonly<{
 export function WorkflowLogo(props: Props) {
   return (
     <img
-      src={`/logos/${props.id}`}
+      src={`/logos/${props.id}.svg`}
       alt={`The logo of the '${props.title}' workflow`}
       className="w-full h-full"
     />
