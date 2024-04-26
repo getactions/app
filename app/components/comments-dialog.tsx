4eeae7e9f3ce1781ef3836b7785a3af7b98ee513
André König <andre.koenig@openformation.io>
2024-04-26 14:51:46 +0200

    feat: improves loading handling of comments

--- app/components/comments-dialog.tsx
+++ app/components/comments-dialog.tsx
@@ -1,6 +1,7 @@
 import Giscus from "@giscus/react";
 
 import { ChatBubbleIcon } from "@radix-ui/react-icons";
+import { LoadingIndicator } from "./loading-indicator";
 import { ResponsiveDialog } from "./responsive-dialog";
 import { Button } from "./ui/button";
 import { WorkflowLogo } from "./workflow-logo";
@@ -27,19 +28,28 @@ export function CommentsDialog({ id, title }: Props) {
         </Button>
       }
     >
-      <div className="py-4 px-2.5 pr-3 max-h-[40vh] overflow-auto">
-        <Giscus
-          repo="getactions/comments"
-          host="https://comments.getactions.dev"
-          repoId="R_kgDOLwjfnA"
-          categoryId="DIC_kwDOLwjfnM4Cezi4"
-          strict="1"
-          mapping="og:title"
-          reactionsEnabled="0"
-          emitMetadata="0"
-          inputPosition="top"
-          lang="en"
-        />
+      {/* We render a loading animation here, which lives under the comments component.
+          When the comments component is ready, it will be overlaid on top of the loading animation. */}
+      <div className="max-h-[40vh] min-h-[320px] overflow-auto relative">
+        <div className="absolute left-0 right-0 top-20 flex items-center justify-center -z-10">
+          <div className="h-16 w-16">
+            <LoadingIndicator />
+          </div>
+        </div>
+        <div className="pl-3.5 py-4 pr-4">
+          <Giscus
+            repo="getactions/comments"
+            host="https://comments.getactions.dev"
+            repoId="R_kgDOLwjfnA"
+            categoryId="DIC_kwDOLwjfnM4Cezi4"
+            strict="1"
+            mapping="og:title"
+            reactionsEnabled="0"
+            emitMetadata="0"
+            inputPosition="bottom"
+            lang="en"
+          />
+        </div>
       </div>
     </ResponsiveDialog>
   );
