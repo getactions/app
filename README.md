f0630c1bb14017762706c20d541ceadddfb3254f
André König <andre.koenig@openformation.io>
2024-05-03 12:25:02 +0200

    chore: adjusts devbox dependencies

--- README.md
+++
@@ -1,7 +0,0 @@
-# getactions.dev
-
-## Copyright
-
-MIT Licensed
-
-2024, [Open Formation GmbH](https://openformation.io), Hamburg, Germany
--- app/components/ui/card.tsx
+++ app/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-xl border bg-card text-card-foreground shadow",
+      "rounded-xl border border-gray-50 bg-card text-card-foreground shadow-md",
       className
     )}
     {...props}
@@ -73,4 +73,5 @@ const CardFooter = React.forwardRef<
 ))
 CardFooter.displayName = "CardFooter"
 
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
+
