eb6638244274221af9c03e95890e56fae4c4e2a5
André König <andre.koenig@openformation.io>
2024-05-06 19:17:16 +0200

    chore: adds correct plausible debug header

--- app/routes/$category.$id/route.tsx
+++ app/routes/$category.$id/route.tsx
@@ -44,7 +44,7 @@ export async function loader({ params, request }: LoaderFunctionArgs) {
             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
           "X-Forwarded-For": request.headers.get("X-Forwarded-For") ?? "",
           "Content-Type": "application/json",
-          "X-Debug-Requested": "true",
+          "X-Debug-Request": "true",
         },
         body: JSON.stringify({
           name: "InstallScriptWasDownloaded",
